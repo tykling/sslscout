da01cf8120359a5a18a19a20a912a970e3ab2e58
Thomas Steen Rasmussen <thomas@gibfest.dk>
2014-04-18 00:46:30 +0200

    small changes, show queue length

--- src/sslscout/context_processors.py
+++ src/sslscout/context_processors.py
@@ -1,4 +1,12 @@
 from django.conf import settings
 
+def queuelength(request):
+    queueitems = CheckQueue.objects.filter(finished=False)
+    userqueue = CheckQueue.objects.filter(user=request.user)
+    return {
+        'queuelength': queueitems.len(),
+        'userqueuelength': userqueue.len()
+    }
+
 def site(request):
     return {'SITE_URL': settings.SITE_URL}
