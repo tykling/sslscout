9fcf4082bdb906debd683435533fae579374ce9d
Thomas Steen Rasmussen <thomas@gibfest.dk>
2014-04-17 11:09:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 sslscout
 ========
 
-sslscout
+About
+-----
+ssl configuration is an important but very complicated and ever changing topic. The complexity of configuring an ssl webserver correctly has lead to the creation of various ssl configuration checking services that help administrators get it right. The problem is that few admins remember to re-check their servers rating periodically. As new SSL vulnerabilities are found, or new and better ciphers are added, the best practices for running a secure ssl website changes, so the rating of a site can change at any time. sslscout is a web application where users can configure periodic checks of their websites using the various online ssl-configuration checking services. Users can configure automatic email notifications if/when the rating changes.
+
+
+Service
+-------
+The service is under development. The latest revision of the master branch of https://github.com/tykling/sslscout/ is always running on https://dev.sslscout.com and eventually the production service will run on https://sslscout.com for all to use.
+
+
+Install
+-------
+If you want to run your own instance of sslscout the source code is freely available under a BSD license. sslscout is a django application originally written for django 1.6. It also requires the django-registration and django-profile packages. To try it out just download the source and run it using djangos own webserver. To run it in production I use [http://projects.unbit.it/uwsgi/ uwsgi].
+
+
+Configuration
+-------------
+Copy settings.py.dist to settings.py and change database and email server settings, and you should be good to go.
+
+
+Questions
+---------
+- On IRC #sslscout on Freenode
+- Make a github issue at https://github.com/tykling/sslscout/issues
+- Send email to info@sslscout.com
+
+
+Author
+------
+idea (oct. '13) and original code (april '14) by Thomas Steen Rasmussen / Tykling <thomas@gibfest.dk>
---
+++ src/sslscout/urls.py
@@ -0,0 +1,43 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    ### admin site
+    url(r'^admin/', include(admin.site.urls)),
+
+    ### static pages
+    url(r'^/$', 'sslscout.views.staticpage',{'page': 'frontpage.html'}),
+
+    ### auth and pw stuff
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, {'next_page': '/accounts/login/'}, name='logout'),
+    url(r'^accounts/password_change/$', password_change, {'template_name': 'registration/password_change.html'}, name='password_change'),
+    url(r'^accounts/password_change_done/$', password_change_done, {'template_name': 'registration/password_change_done.html'}, name='password_change_done'),
+    url(r'^accounts/password_reset/$', password_reset, {'template_name': 'registration/password_reset.html'}, name='password_reset'),
+    url(r'^accounts/password_reset_done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^accounts/password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^accounts/password_reset_complete/$', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name='password_reset_complete'),
+    url(r'^accounts/profile/$', RedirectView.as_view(url='/profile/', permanent=True), name='profile'),
+
+    ### registration stuff
+    url(r'^accounts/activate/complete/$', TemplateView.as_view(template_name='registration/activation_complete.html'), name='registration_activation_complete'),
+    url(r'^accounts/activate/(?P<activation_key>\w+)/$', ActivationView.as_view(), name='registration_activate'),
+    url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),name='registration_register'),
+    url(r'^register/complete/$', TemplateView.as_view(template_name='registration/registration_complete.html'), name='registration_complete'),
+    url(r'^register/closed/$', TemplateView.as_view(template_name='registration/registration_closed.html'), name='registration_disallowed'),
+    
+    ### profile stuff
+    url(r'^profile/$', 'sslscout.views.profile'),
+    url(r'^profile/edit/$', 'sslscout.views.profile_edit'),    
+
+    ### site and sitegroup related stuff
+    url(r'^sites/$', 'sslscout.views.sites'),
+    url(r'^sites/add/$', 'sslscout.views.add_site'),
+    url(r'^sites/edit/(?P<siteid>\w+)/$', 'sslscout.views.edit_site'),
+    url(r'^sites/delete/(?P<siteid>\w+)/$', 'sslscout.views.delete_site'),
+    url(r'^sitegroups/$', 'sslscout.views.sitegroups'),
+    url(r'^sitegroups/add/$', 'sslscout.views.add_sitegroup'),
+    url(r'^sitegroups/edit/(?P<siteid>\w+)/$', 'sslscout.views.edit_sitegroup'),
+    url(r'^sitegroups/delete/(?P<siteid>\w+)/$', 'sslscout.views.delete_sitegroup'),    
+)
