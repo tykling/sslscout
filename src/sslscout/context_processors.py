e4f348c58f02cafe04b9ed797260336664302b08
Thomas Steen Rasmussen <thomas@gibfest.dk>
2014-04-18 01:12:14 +0200

    more navbar changes

--- src/sslscout/context_processors.py
+++ src/sslscout/context_processors.py
@@ -2,11 +2,17 @@ from sslscout.models import CheckQueue, Site, SiteGroup
 from django.conf import settings
 
 def queuelength(request):
-    queuelength = CheckQueue.objects.filter(finished=False).count()
-    userqueuelength = CheckQueue.objects.filter(site__sitegroup__user=request.user).count()
-    sitegroupcount = SiteGroup.objects.filter(user=request.user).count()
-    sitecount = Site.objects.filter(sitegroup__user=request.user).count()
-
+    if user.is_authenticated:
+        queuelength = CheckQueue.objects.filter(finished=False).count()
+        userqueuelength = CheckQueue.objects.filter(site__sitegroup__user=request.user).count()
+        sitegroupcount = SiteGroup.objects.filter(user=request.user).count()
+        sitecount = Site.objects.filter(sitegroup__user=request.user).count()
+    else:
+        queuelength = 0
+        userqueuelength = 0
+        sitegroupcount = 0
+        sitecount = 0
+    
     return {
         'queuelength': queuelength,
         'userqueuelength': userqueuelength,
