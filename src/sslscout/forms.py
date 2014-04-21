4ddc1ec0a5929dc5531421049c951284390acc26
Thomas Steen Rasmussen <thomas@gibfest.dk>
2014-04-21 21:33:46 +0200

    still working on interval_hours validation

--- src/sslscout/forms.py
+++ src/sslscout/forms.py
@@ -17,21 +17,22 @@ class SiteGroupForm(forms.ModelForm):
     def clean(self):
         ### get interval_hours from cleaned_data
         cleaned_data = super(SiteGroupForm, self).clean()
-        interval_hours = cleaned_data.get("interval_hours")
+        if 'interval_hours' in cleaned_data:
+            interval_hours = cleaned_data.get("interval_hours")
 
-        ### check if interval_hours is numeric
-        try:
-            temp = float(interval_hours)
-        except:
-            self._errors["interval_hours"] = self.error_class([u"Invalid input"])
-            del cleaned_data["interval_hours"]
-            return cleaned_data
-        
-        ### check if interval_hours is lower than the limit
-        if interval_hours < 24:
-            self._errors["interval_hours"] = self.error_class([u"The lowest permitted check interval is 24 hours"])
-            del cleaned_data["interval_hours"]
-            return cleaned_data
+            ### check if interval_hours is numeric
+            try:
+                temp = float(interval_hours)
+            except:
+                self._errors["interval_hours"] = self.error_class([u"Invalid input"])
+                del cleaned_data["interval_hours"]
+                return cleaned_data
+            
+            ### check if interval_hours is lower than the limit
+            if interval_hours < 24:
+                self._errors["interval_hours"] = self.error_class([u"The lowest permitted check interval is 24 hours"])
+                del cleaned_data["interval_hours"]
+                return cleaned_data
             
         ### return the cleaned data.
         return cleaned_data
