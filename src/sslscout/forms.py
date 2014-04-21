b0ef51a10f8ae383b82e546bf0a194577e9e4fc4
Thomas Steen Rasmussen <thomas@gibfest.dk>
2014-04-21 21:30:09 +0200

    improve validation of interval_hours
    fixes #4, fixes #5

--- src/sslscout/forms.py
+++ src/sslscout/forms.py
@@ -15,14 +15,24 @@ class SiteGroupForm(forms.ModelForm):
         fields = ('name','interval_hours','alert')
 
     def clean(self):
+        ### get interval_hours from cleaned_data
         cleaned_data = super(SiteGroupForm, self).clean()
-
-        ### check that interval_hours is 24 or more
         interval_hours = cleaned_data.get("interval_hours")
-        if interval_hours < 24:
-            self._errors["interval_hours"] = self.error_class([u"The lowest permitted check interval is 24 hours."])
+
+        ### check if interval_hours is numeric
+        try:
+            temp = float(interval_hours)
+        except:
+            self._errors["interval_hours"] = self.error_class([u"Invalid input"])
             del cleaned_data["interval_hours"]
+            return cleaned_data
         
+        ### check if interval_hours is lower than the limit
+        if interval_hours < 24:
+            self._errors["interval_hours"] = self.error_class([u"The lowest permitted check interval is 24 hours"])
+            del cleaned_data["interval_hours"]
+            return cleaned_data
+            
         ### return the cleaned data.
         return cleaned_data
 
