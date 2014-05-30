8c2762480a1d7950885a0f6dc9b198a73de8e4d9
Thomas Steen Rasmussen <thomas@gibfest.dk>
2014-05-30 19:24:00 +0200

    remove some output from runengines.py

--- src/sslscout/management/commands/runengines.py
+++ src/sslscout/management/commands/runengines.py
@@ -33,7 +33,7 @@ class Command(BaseCommand):
         engines = CheckEngine.objects.filter(active=True)
         enginethreads = []
         for engine in engines:
-            print "############ Working on engine %s" % engine.name
+            #print "############ Working on engine %s" % engine.name
             ### check if this engine already has a job running
             if SiteCheck.objects.filter(finish_time=None,engine=engine).count() > 0:
                 ### skipping this engine
@@ -43,7 +43,7 @@ class Command(BaseCommand):
             ### find a site that needs checking
             sites = Site.objects.all()
             for site in sites:
-                print "############ Working on site %s" % site.hostname
+                #print "############ Working on site %s" % site.hostname
 
                 try:
                     ### find the latest sitecheck for this hostname with this engine
@@ -56,11 +56,11 @@ class Command(BaseCommand):
                 if latest_sitecheck:
                     if latest_sitecheck.finish_time + timedelta(hours=site.sitegroup.interval_hours) > timezone.now():
                         ### not yet
-                        print "############ this site does not need to be checked yet, skipping..."
+                        #print "############ this site does not need to be checked yet, skipping..."
                         continue
 
                 ### OK, time to do a new check for this site
-                print "############ starting new sitecheck thread for this site..."
+                print "############ starting new sitecheck thread for site %s with engine %s" % (site.hostname,engine.name)
 
                 sitecheck = SiteCheck(hostname=site.hostname,engine=engine)
                 sitecheck.save()
