ae40d04bee20b37120a9bc8692a957a2c430b24f
Thomas Steen Rasmussen <thomas@gibfest.dk>
2014-05-30 19:13:50 +0200

    small fixes to engines.py

--- src/sslscout/engines.py
+++ src/sslscout/engines.py
@@ -76,11 +76,7 @@ class www_ssllabs_com(threading.Thread):
                     ### get the results
                     summarydiv = parsed_html.find_all('div', attrs={'class': 'sectionTitle'},text='Summary')[0].parent
 
-                    if summarydiv.find('div',attrs={'class': 'rating_g'}):
-                        result.overall_rating = summarydiv.find('div',attrs={'class': 'rating_g'}).text
-                    else:
-                        result.overall_rating = summarydiv.find('div',attrs={'class': 'rating_r'}).text
-
+                    result.overall_rating = find('div',attrs={'class': 'ratingTitle'}).findNextSiblings('div')[0].text
                     result.certificate_score = int(summarydiv.find('div',attrs={'class': 'chartLabel'},text='Certificate').parent.find('div',attrs={'class': 'chartValue'}).text)
                     result.protocolsupport_score = int(summarydiv.find('div',attrs={'class': 'chartLabel'},text='Protocol Support').parent.find('div',attrs={'class': 'chartValue'}).text)
                     result.keyexchange_score = int(summarydiv.find('div',attrs={'class': 'chartLabel'},text='Key Exchange').parent.find('div',attrs={'class': 'chartValue'}).text)
@@ -111,7 +107,7 @@ class www_ssllabs_com(threading.Thread):
 
                         result.serverip = parsed_html.find('div', attrs={'class': 'reportTitle'}).find('span',attrs={'class': 'ip'}).text.strip()[1:-1]
                         summarydiv = parsed_html.find_all('div', attrs={'class': 'sectionTitle'},text='Summary')[0].parent
-                        result.overall_rating = summarydiv.find('div',attrs={'class': 'rating_g'}).text
+                        result.overall_rating = find('div',attrs={'class': 'ratingTitle'}).findNextSiblings('div')[0].text
                         result.certificate_score = int(summarydiv.find('div',attrs={'class': 'chartLabel'},text='Certificate').parent.find('div',attrs={'class': 'chartValue'}).text)
                         result.protocolsupport_score = int(summarydiv.find('div',attrs={'class': 'chartLabel'},text='Protocol Support').parent.find('div',attrs={'class': 'chartValue'}).text)
                         result.keyexchange_score = int(summarydiv.find('div',attrs={'class': 'chartLabel'},text='Key Exchange').parent.find('div',attrs={'class': 'chartValue'}).text)
